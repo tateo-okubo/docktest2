a7e0245f9459b08198ebc5389251e9c61954b87e
tateo-okubo <tateo-okubo@united.jp>
2020-01-20 17:56:06 +0900

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 
 echo "====== end pip install ======"
 
-if [ -n "$GITHUB_ACTIONS" ]; then
+if [ -z "$GITHUB_ACTIONS" ]; then
 mkdocs serve --dev-addr=0.0.0.0:8000
 else
 mkdocs build
